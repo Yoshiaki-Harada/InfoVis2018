6de8c51b4423165598cfa442481f08343fa28259
原田　佳明 <183x022x@istc-a-041.epc.center.kobe-u.ac.jp>
2018-04-18 14:22:49 +0900

    Initisl commit

---
+++ main.js
@@ -0,0 +1,38 @@
+function main()
+{
+    var width = 500;
+    var height = 500;
+
+    //シーン作成
+    var scene = new THREE.Scene();
+
+    var fov = 45;
+    var aspect = width / height;
+    var near = 1;
+    var far = 1000;
+    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set( 0, 0, 5 );
+    scene.add( camera );
+
+    //レンダラーを作成
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+
+    //箱を作成
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+
+    loop();
+
+    function loop()
+    {
+        requestAnimationFrame( loop );
+        cube.rotation.x += 0.001;
+        cube.rotation.y += 0.001;
+        renderer.render( scene, camera );
+    }
+}
